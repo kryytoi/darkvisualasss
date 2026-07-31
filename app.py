6b4853dac2c593a65a44c40b5dc29a7480398359
kryytoi <maksimklysa80@gmail.com>
2026-07-31 09:26:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -554,5 +554,5 @@ def admin_action():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)ы
+    app.run(host="0.0.0.0", port=port, debug=True)
                 
