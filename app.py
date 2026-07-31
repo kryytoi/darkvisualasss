a0e558c97b046010322b8aa4d62e2245bc5b26c5
kryytoi <maksimklysa80@gmail.com>
2026-07-31 03:19:07 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -281,4 +281,4 @@ def admin_activate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", os.environ.get("DEV_PORT", 3000))))
